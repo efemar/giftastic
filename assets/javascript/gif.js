e0057dde019391cab694da4c657ff33b65ff87b5
Carmen SanDiego <efemar82@gmail.com>
2019-09-10 15:57:49 -0700

    added function for displaying buttons

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -1,72 +1,57 @@
 $(document).ready(function () {
 
     // Initial array of places
-    var travelPlaces = ["Argentina", "Brazil", "Canada", "Costa Rica", "Colombia", "Chile", "Peru", "Uruguay", "Germany", "Scotland", "Ireland", ""]
+    var travelPlaces = ["Argentina", "Brazil", "Canada", "Costa Rica", "Colombia", "Chile", "Peru", "Uruguay", "Germany", "Scotland", "Ireland"]
 
 
-    $("#submit-gif").on("click", function (event) {
-        event.preventDefault();
-        var searchGif = $("#inputgif").val();
-        getGif(searchGif)
+    //Take each string from the array above and create buttons in the html output
 
-        console.log(getGif);
+    function createButtons() {
 
+        $("#buttons-view").empty();
 
-        // This function handles events where the search gifs button is clicked
-
-        function getGif(searchGif) {
-
-            var queryURL =
-                "https://api.giphy.com/v1/gifs/" +
-                searchGif +
-                "?api_key=knpLJPTetvVyzRcKfITKxGEP3gebgMJE&q=&limit=10&offset=0&rating=G&lang=en";
-
-            console.log(queryURL);
-
-            $.ajax({
-                url: queryURL,
-                method: "GET"
-            }).then(function (response) {
-                console.log(response)
-
-
-            })
-
+        for (var i = 0; i < travelPlaces.length; i++) {
+            var a = $("<button>");
+            a.attr("type", "button")
+            a.addClass("place");
+            a.addClass("btn btn-primary btn-sm");
+            a.attr("data-place", travelPlaces[i]);
+            a.text(travelPlaces[i]);
+            $("#gifs").append(a);
         }
 
+    }
 
 
+    //The button needs to grab information from each of the topics from giphy and displaying 10 static non animated gof images 
 
+    $("#add-gif").on("click", function (event) {
+        event.preventDefault();
 
+        var travelPlace = $("#gif-input").val().trim();
 
-        //Make a request with Ajax
-
-
-        //Retrieve rating (PG, G, so on), title, tags, etc)
-
-
-
+        travelPlaces.push(travelPlace);
     })
 
+    createButtons();
+        
+    
+
+    // Display the images on the page
+
+
+
+    //Display lay-out: Rating (G, ) + metadata (title, tags, etc) 
+
+
+
+    // Create a search field that allows users to add their own custom searches
+
+
+
+
+
+
+    //Look at 07-firebase for button issue and clicks
+
 })
-        //Change status of gif from still to animate
-
-        //Display data to the output div
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
